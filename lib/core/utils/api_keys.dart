3df6e0166ab17f3f60ba2230c4013ef543723c23
AyaNasserEzat <125359511+AyaNasserEzat@users.noreply.github.com>
2024-09-08 10:03:59 +0300

    Update api_keys.dart

--- lib/core/utils/api_keys.dart
+++ lib/core/utils/api_keys.dart
@@ -1,7 +1,7 @@
 class StripKeys {
   static String secretKey =
-      'sk_test_51Psi7BKs9AKBLgVtYX8DOyIeBWJYSDWD1NCLicknfDgp3fwBb9xtIIzZfiWLdz6rkGMwpbmXB8jHivQEu8ujIiId00ILqPCWiJ';
+      '';
 
   static String publishableKey =
-      'pk_test_51Psi7BKs9AKBLgVtsIRxB4MzRXT1OPCvEzFGDD70Vd5ab4QnqtmruwiK5qCgmr6T5m2YMjX3VnClvraBEefaBAyf00rQ65zeEX';
+      '';
 }
